a26ac617c686bd7f81d21af9fc8b9ff9eeeb1d7c
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-05-03 02:02:46 -1000

    starting the long haul

--- app.py
+++ app.py
@@ -5,4 +5,34 @@ Drew Gotshalk
 Charnalyn Crivello
 2020-04-09
 """
+import sys
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/shoestore.db'
+database=SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def login(name):
+    return name
+
+
+@app.route('/shoes')
+def shoes():
+    return render_template('shoes.html')
+
+@app.route('/socks')
+def socks():
+    return render_template('socks.html')
+
+@app.route('/accessories')
+def accessories():
+    return render_template('accessories.html')
+if __name__ == "__main__":
+    app.run()
