9d761fc567918f10dc234745c56349bc69c6cc68
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-05-03 10:14:09 -1000

    working sock,shoe,accessory section

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ Charnalyn Crivello
 2020-04-09
 """
 import sys
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+sys.path.insert(1, './database')
+from create_store import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/shoestore.db'
@@ -16,23 +18,79 @@ database=SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/user/<name>')
-def login(name):
-    return name
+    return render_template('index.html',Item_type="Shoe Store", items=['1','2','3'])
 
 
 @app.route('/shoes')
 def shoes():
-    return render_template('shoes.html')
+    return render_template('shoes.html',Item_type="Shoes", items=shoedisplay())
+
+@app.route('/shoes',methods=["POST"])
+def get_shoe():
+    quantity = request.form['quantity']
+    item_id = request.form['itemid']
+
+    return redirect(url_for('shoes'))
+
+
 
 @app.route('/socks')
-def socks():
-    return render_template('socks.html')
+def socks():    
+    return render_template('socks.html',Item_type="Socks", items=sockdisplay())
+
+@app.route('/socks',methods=['POST'])
+def get_socks():
+    quantity = request.form['quantity']
+    item_id = request.form['itemid']
+    return redirect(url_for('socks'))
 
 @app.route('/accessories')
-def accessories():
-    return render_template('accessories.html')
+def Accessories():
+    return render_template('accessories.html',Item_type="Accessories", items=accessorydisplay())
+
+@app.route('/accessories',methods=["POST"])
+def get_accessories():
+    quantity = request.form['quantity']
+    item_id = request.form['itemid']
+    return redirect(url_for('socks'))
+
+
+
+def shoedisplay():
+    a_query = database.session.query(Shoes.c.SHOE_NAME)\
+            .add_columns(Item.c.PRICE,
+                    Shoes.c.SHOE_TYPE,
+                    Item.c.BRAND,
+                    Item.c.ITEM_ID)\
+            .add_column(Shoes.c.SHOE_DESC)\
+            .filter(Item.c.ITEM_ID == Shoes.c.SHOE_ID)
+    return a_query
+
+
+def sockdisplay(): 
+    a_query = database.session.query(Socks.c.SOCK_NAME)\
+            .add_columns(Item.c.PRICE,
+                    Item.c.BRAND,
+                    Item.c.ITEM_ID)\
+            .add_column(Socks.c.SOCK_DESC)\
+            .filter(Item.c.ITEM_ID == Socks.c.SOCK_ID)
+    return a_query
+
+
+def accessorydisplay():
+    a_query = database.session.query(Accessory.c.ACC_NAME)\
+            .add_columns(Item.c.PRICE,
+                    Item.c.BRAND,
+                    Item.c.ITEM_ID)\
+            .add_column(Accessory.c.ACC_DESC)\
+            .filter(Item.c.ITEM_ID == Accessory.c.ACC_ID)
+    return a_query
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
--- database/create_store.py
+++ database/create_store.py
@@ -5,7 +5,7 @@ from csv import DictReader
 metadata = MetaData()
 engine = create_engine('sqlite:///shoestore.db')
 
-customer = Table('CUSTOMER', metadata,
+CUSTOMER = Table('CUSTOMER', metadata,
         Column('CUST_ID', Integer(),primary_key=True), 
         Column('EMAIL', String(50), nullable=False, unique=True),
         Column('PHONE_NUM',  Integer()),
@@ -13,19 +13,19 @@ customer = Table('CUSTOMER', metadata,
         Column('PASSWORD', String(25), nullable=False),
         Column('PAYMENT_TYPE',String(16),index=True))
  
-cust_name = Table('CUST_NAME', metadata,
+CUST_NAME = Table('CUST_NAME', metadata,
         Column('CUST_ID', Integer(), ForeignKey('CUSTOMER.CUST_ID'), primary_key=True ),
         Column('FNAME',String(25),nullable=False),
         Column('MNAME',String(25),nullable=True),
         Column('LNAME', String(25),nullable=False))
 
-payment_type = Table('PAYMENT_TYPE', metadata,
+PAYMENT_TYPE = Table('PAYMENT_TYPE', metadata,
         Column('CUST_ID', Integer(), ForeignKey('CUSTOMER.CUST_ID'), primary_key=True),
         Column('GIFTCARD', String(16)),
         Column('DEBIT',  String(16)),
         Column('CREDIT',String(16)))
 
-order = Table('ORDER', metadata,
+ORDER = Table('ORDER', metadata,
         Column('CUST_ID', Integer(), ForeignKey('CUSTOMER.CUST_ID'), primary_key=True),
         Column('ORDER_ID', Integer(), primary_key=True),
         Column('ORDER_TYPE', Integer(), nullable=False),
@@ -33,34 +33,34 @@ order = Table('ORDER', metadata,
         Column('ORDER_DATE', String(20), nullable=False, index=True),
         Column('PAYMENT_AMOUNT', Float(), nullable=False))
 
-item = Table('ITEM', metadata,
+ITEM = Table('ITEM', metadata,
         Column('ITEM_ID', Unicode(20), primary_key=True),
         Column('PRICE', Float(8), nullable=False, index=True),
         Column('BRAND', String(50), nullable=False),
         Column('TYPE', String(10), nullable=False))
 
-accessory = Table('ACCESSORY', metadata,
+ACCESSORY = Table('ACCESSORY', metadata,
         Column('ACC_ID', Unicode(20), ForeignKey('ITEM.ITEM_ID'), primary_key=True),
         Column('ACC_DESC', String(100)),
         Column('ACC_NAME', String(50), nullable=False))
 
-shoes = Table('SHOES', metadata,
+SHOES = Table('SHOES', metadata,
         Column('SHOE_ID', Unicode(20), ForeignKey('ITEM.ITEM_ID'), primary_key=True),
         Column('SHOE_DESC', String(100)),
         Column('SHOE_NAME', String(50), nullable=False),
         Column('SHOE_TYPE', String(50), nullable=True))
 
-socks = Table('SOCKS', metadata,
+SOCKS = Table('SOCKS', metadata,
         Column('SOCK_ID', Unicode(20), ForeignKey('ITEM.ITEM_ID'), primary_key=True),
         Column('SOCK_DESC', String(100)),
         Column('SOCK_NAME', String(50), nullable=False))
 
-contains = Table('CONTAINS', metadata,
+CONTAINS = Table('CONTAINS', metadata,
         Column('CUST_ID', Integer(), ForeignKey('CUSTOMER.CUST_ID'), primary_key=True),
         Column('ITEM_ID', Unicode(8), ForeignKey('ITEM.ITEM_ID'), primary_key=True))
         
 
-cart = Table('CART', metadata,
+CART = Table('CART', metadata,
         Column('CUST_ID', Integer(), ForeignKey('CUSTOMER.CUST_ID'), primary_key=True),
         Column('ITEM_ID', Unicode(8), ForeignKey('ITEM.ITEM_ID'), primary_key=True),
         Column('NUM_ITEM', Integer(), nullable=True))
